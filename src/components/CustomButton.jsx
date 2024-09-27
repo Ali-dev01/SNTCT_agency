bc2845dfda2738989f9e9b54dd6028e29e4ed4fc
ali <alidev098@gmail.com>
2024-09-27 22:57:06 +0500

    update custom button

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,9 +1,19 @@
-import { Button } from "@mui/material";
+import ArrowUp from "@/icons/ArrowUp";
+import { Box, Button } from "@mui/material";
 
 const CustomButton = () => {
   return (
     <>
-      <Button variant="contained" color="primary" sx={styles.buttonStyle}>
+      <Button
+        variant="contained"
+        color="primary"
+        sx={styles.buttonStyle}
+        endIcon={
+          <Box sx={styles.iconStyle}>
+            <ArrowUp />
+          </Box>
+        }
+      >
         Book a Meeting
       </Button>
     </>
@@ -13,16 +23,26 @@ export default CustomButton;
 
 const styles = {
   buttonStyle: (theme) => ({
-    py: "16px",
-    px: "24px",
+    p: "10px 24px",
+    pr: "14px",
     boxShadow: "none",
     background: `linear-gradient(45deg, ${theme.palette.primary.main}, ${theme.palette.primary.main})`,
     textTransform: "none",
-    borderRadius: "32px",
+    borderRadius: "35px",
     transition: "background 0.4s ease, transform 0.2s ease", // Smooth transition
     "&:hover": {
       background: theme.palette.gradient.main, // Full gradient on hover
       boxShadow: "none",
     },
+  }),
+  iconStyle: (theme) => ({
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center",
+    background: theme.palette.common.white,
+    width:"34px",
+    height:"34px",
+    borderRadius:"100%",
+    marginLeft:"20px"
   }),
 };
---
+++ src/icons/ArrowUp.jsx
@@ -0,0 +1,20 @@
+const ArrowUp = () => {
+  return (
+    <svg
+      width={12}
+      height={11}
+      viewBox="0 0 12 11"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10.7152 10.2147V0.786621M10.7152 0.786621H1.28711M10.7152 0.786621L1.28757 10.2143"
+        stroke="#3395FF"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+export default ArrowUp;
